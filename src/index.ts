ffe676c85e1aea6891306ad600cd0d1affdea154
lucas-laza <laza.lucas15@gmail.com>
2024-03-05 16:45:48 +0100

    q7

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,34 @@
 import { readFileSync } from "fs";
 
-function getStatistics(): {meanAge: number, meanHeight: number} {
-  const persons: unknown[] = JSON.parse(readFileSync("./persons.json").toString());
+type Person = { age: number; height: number };
+
+function getStatistics(): { meanAge: number, meanHeight: number } {
+  const persons: Person[] = JSON.parse(readFileSync("./persons.json").toString());
 
   let meanAge: number = 0;
   let meanHeight: number = 0;
   
   for (let index = 0; index < persons.length; index++) {
+    if (
+      !Object.hasOwn(persons[index], 'age') || 
+      typeof persons[index].age !== 'number' || 
+      isNaN(persons[index].age) ||
+      !Object.hasOwn(persons[index], 'height') ||
+      typeof persons[index].height !== 'number' || 
+      isNaN(persons[index].height)
+    ) {
+      throw new Error("Wrong data");
+    }
     meanAge += persons[index].age;
     meanHeight += persons[index].height;
   }
 
-  meanAge = meanAge / persons.length
-  meanHeight = meanHeight / persons.length
+  meanAge = meanAge / persons.length;
+  meanHeight = meanHeight / persons.length;
 
   return {
-    meanAge: meanAge,
-    meanHeight: meanHeight
+    meanAge,
+    meanHeight
   };
 }
 
