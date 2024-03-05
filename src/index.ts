a4d45f295eab8b82cc76ee3b8f29b384dec3c18a
lucas-laza <laza.lucas15@gmail.com>
2024-03-05 16:20:28 +0100

    q3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { readFileSync } from "fs";
 
 function getStatistics(): number {
   const persons: {"age": number, "height": number}[] = JSON.parse(readFileSync("./persons.json").toString());
+
+  let maxAge: number = 0;
+  persons.forEach(person => {
+    if (person.age !== undefined && person.age > maxAge) {
+      maxAge = person.age;
+    }
+  });
+
+  return maxAge;
 }
 
 function displayResult() {
